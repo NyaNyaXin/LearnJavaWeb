ba2fd494fbd1fdb991bd59ee147790b419964154
NyaNyaXin <chenxin95013>
2017-04-30 15:12:48 +0800

    JavaWeb学习之ServletContext

--- Day01/src/com/cx/javaweb/HelloServlet.java
+++ Day01/src/com/cx/javaweb/HelloServlet.java
@@ -1,10 +1,12 @@
 package com.cx.javaweb;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -42,6 +44,43 @@ public class HelloServlet implements Servlet {
 		
 		String servletName = servletConfig.getServletName();
 		System.out.println(servletName);
+		
+		//获取ServletContext对象
+		ServletContext servletContext = servletConfig.getServletContext();
+		
+		String driver = servletContext.getInitParameter("driver");
+		System.out.println("driver"+driver);
+		
+		Enumeration<String> names2 = servletContext.getInitParameterNames();
+		while(names2.hasMoreElements()){
+			String name = names2.nextElement();
+			System.out.println("-->"+name);
+		}
+		
+		String path  = servletContext.getRealPath("/note.txt");
+		System.out.println(path);
+		
+		String contextpath = servletContext.getContextPath();
+		System.out.println(contextpath);
+		
+		try {
+			ClassLoader classLoader = getClass().getClassLoader();
+			InputStream is = classLoader.getResourceAsStream("jdbc.properties");
+			System.out.println("1." + is);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			InputStream is2 = servletContext.getResourceAsStream("/WEB-INF/classes/jdbc.properties");
+			System.out.println("2." + is2);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Override
