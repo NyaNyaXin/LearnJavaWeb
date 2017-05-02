4fb2c254e20878cf59fcba0f090bc6ca83f5c3c7
NyaNyaXin <chenxin95013>
2017-05-02 11:32:14 +0800

    JavaWeb学习之Servlet小节

---
+++ Servlet02/src/com/cx/javaweb/LoginServlet3.java
@@ -0,0 +1,90 @@
+package com.cx.javaweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet3 extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		
+		
+		PrintWriter out =resp.getWriter();
+		
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql:///javaweb";
+			String user = "root";
+			String sqlpassword = "123456";
+			connection = DriverManager.getConnection(url, user, sqlpassword);
+			String sql = "SELECT COUNT(id) FROM users WHERE username=? AND password=?";
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, username);
+			statement.setString(2, password);
+			
+			resultSet = statement.executeQuery();
+			if(resultSet.next()){
+				int count = resultSet.getInt(1);
+				if(count>0){
+					out.print("Hello:"+ username);
+				}else{
+					out.print("Sorry:"+ username);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				if(resultSet!=null){
+					resultSet.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				if(statement!=null){
+					statement.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				if(connection!=null){
+					connection.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+}
