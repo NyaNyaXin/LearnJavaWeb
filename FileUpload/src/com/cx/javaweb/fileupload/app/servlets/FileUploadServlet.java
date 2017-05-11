10a38e4462277b824878be8077e6d99c6c70e01e
NyaNyaXin <chenxin95013>
2017-05-11 19:28:18 +0800

    不写了

--- FileUpload/src/com/cx/javaweb/fileupload/app/servlets/FileUploadServlet.java
+++ FileUpload/src/com/cx/javaweb/fileupload/app/servlets/FileUploadServlet.java
@@ -30,24 +30,24 @@ public class FileUploadServlet extends HttpServlet {
 		ServletFileUpload upload = getServletFileUpload();
 
 		try {
-			//把需要上传的FileItem都放入到该Map中
-			//键：文件待存放的路径，值：对应的FileItem对象
+			// 把需要上传的FileItem都放入到该Map中
+			// 键：文件待存放的路径，值：对应的FileItem对象
 			Map<String, FileItem> uploadFiles = new HashMap<String, FileItem>();
-			//解析请求，得到FileItem的集合
+			// 解析请求，得到FileItem的集合
 			List<FileItem> items = upload.parseRequest(request);
-			
-			//1.构建FileUploadBean的集合，同时填充uploadFiles
-			List<FileUploadBean> beans = buildFileUploadBeans(items,uploadFiles);
-			
-			//2.校验扩展名
+
+			// 1.构建FileUploadBean的集合，同时填充uploadFiles
+			List<FileUploadBean> beans = buildFileUploadBeans(items, uploadFiles);
+
+			// 2.校验扩展名
 			validateExtName(beans);
-			
-			//3.校验文件的大小：在解析时已经校验了，我们只需要通过异常得到结果
-			
-			//4.进行文件的上传操作
+
+			// 3.校验文件的大小：在解析时已经校验了，我们只需要通过异常得到结果
+
+			// 4.进行文件的上传操作
 			upload(uploadFiles);
 
-			//5.把上传的信息保存到数据库中
+			// 5.把上传的信息保存到数据库中
 			savedBeans(beans);
 		} catch (FileUploadException e) {
 			// TODO Auto-generated catch block
@@ -58,21 +58,40 @@ public class FileUploadServlet extends HttpServlet {
 
 	private void savedBeans(List<FileUploadBean> beans) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void upload(Map<String, FileItem> uploadFiles) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void validateExtName(List<FileUploadBean> beans) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private List<FileUploadBean> buildFileUploadBeans(List<FileItem> items, Map<String, FileItem> uploadFiles) {
-		// TODO Auto-generated method stub
+		// 1.遍历FileItem的集合。先得到desc的Map<String,String>,其中键：filedName(desc1...,desc2...)
+		// 值：表单域对应字段的值
+		Map<String, String> descs = new HashMap<>();
+
+		for (FileItem item : items) {
+			if (item.isFormField()) {
+				descs.put(item.getFieldName(), item.getString());
+			}
+		}
+		// 2.再次遍历FileItem集合，得到文件域的FileItem对象
+		// 每得到一个FileItem对象，都创建一个FileUploadBean对象
+		// 得到fileName,构建filePath,从1的Map中得到当前FileItem对应的那个desc
+		// 使用filedName后面的数字去匹配
+		for (FileItem item : items) {
+			if (!item.isFormField()) {
+				String fieldName = item.getFieldName();
+				String index = fieldName.substring(fieldName.length()-1);
+				String fileName = item.getName();
+			}
+		}
 		return null;
 	}
 
